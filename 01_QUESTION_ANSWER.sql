cf571b4daf4fdbca3aff7c6420ee4c3b6b132859
MD-TANZIM-EHSAN <mdtanzime@gmail.com>
2025-06-24 19:28:27 +0600

    Add files via upload

---
+++ 01_QUESTION_ANSWER.sql
@@ -0,0 +1,6 @@
+-- QUESTION  1:-
+--              Total Employees: How many employees are there in the company?
+-- ANSWER :-
+select 
+    count(`Emp ID`) TOTAL_Employees
+from employee_information
